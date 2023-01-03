bf06577ded4b70391ed0f9b845a93572470e8fba
Troy Lee <troylee@berkeley.edu>
2023-01-02 18:08:06 -0800

    bookings seed data

--- backend/db/seeders/20230103020256-demo-bookings.js
+++ backend/db/seeders/20230103020256-demo-bookings.js
@@ -1,25 +1,46 @@
 'use strict';
 
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;  // define your schema in options object
+}
+
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+    options.tableName = 'Bookings'
+
+     await queryInterface.bulkInsert(options, [
+      {
+        spotId:1,
+        userId:1,
+        startDate: '2021-11-25',
+        endDate:'2021-11-28'
+      },
+      {
+        spotId:2,
+        userId:2,
+        startDate: '2021-12-25',
+        endDate:'2021-12-31'
+      },
+      {
+        spotId:3,
+        userId:3,
+        startDate: '2021-11-30',
+        endDate:'2021-12-10'
+      }
+
+    ], {});
+
   },
 
   async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
+    options.tableName = 'Bookings'
+    const Op = Sequelize.Op
+
+    await queryInterface.bulkDelete(options, {
+      spotId: { [Op.in] : [1, 2, 3]}
+    }, {});
+
   }
 };
