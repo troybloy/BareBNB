5674a10a7be049b3d4ebade936c77276197318d4
Troy Lee <troylee@berkeley.edu>
2023-01-02 18:18:49 -0800

    more seeding

--- backend/db/seeders/20230103020256-demo-bookings.js
+++ backend/db/seeders/20230103020256-demo-bookings.js
@@ -28,6 +28,18 @@ module.exports = {
         userId:3,
         startDate: '2021-11-30',
         endDate:'2021-12-10'
+      },
+      {
+        spotId:4,
+        userId:4,
+        startDate: '2021-10-25',
+        endDate:'2021-10-27'
+      },
+      {
+        spotId:5,
+        userId:5,
+        startDate: '2021-11-15',
+        endDate:'2021-11-20'
       }
 
     ], {});
@@ -39,7 +51,7 @@ module.exports = {
     const Op = Sequelize.Op
 
     await queryInterface.bulkDelete(options, {
-      spotId: { [Op.in] : [1, 2, 3]}
+      spotId: { [Op.in] : [1, 2, 3, 4, 5]}
     }, {});
 
   }
--- backend/db/seeders/20230103020401-demo-spotImages.js
+++ backend/db/seeders/20230103020401-demo-spotImages.js
@@ -1,25 +1,65 @@
 'use strict';
+const bcrypt = require("bcryptjs");
+
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;  // define your schema in options object
+}
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+    options.tableName = 'SpotImages';
+    return queryInterface.bulkInsert(options, [
+      {
+        spotId: 1,
+        url: "spot-1-picture-1",
+        preview: true
+      },
+      {
+        spotId: 1,
+        url: "spot-1-picture-2",
+        preview: false
+      },
+      {
+        spotId: 2,
+        url: "spot-2-picture-1",
+        preview: true
+      },
+      {
+        spotId: 2,
+        url: "spot-2-picture-2",
+        preview: false
+      },
+      {
+        spotId: 3,
+        url: "spot-3-picture-1",
+        preview: true
+      },
+      {
+        spotId: 3,
+        url: "spot-3-picture-2",
+        preview: false
+      },
+      {
+        spotId: 4,
+        url: "spot-4-picture-1",
+        preview: true
+      },
+      {
+        spotId: 5,
+        url: "spot-5-picture-1",
+        preview: false
+      }
+
+    ], {});
   },
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
+  down: async (queryInterface, Sequelize) => {
+    options.tableName = 'SpotImages';
+    const Op = Sequelize.Op;
+    return queryInterface.bulkDelete(options, {
+      spotId: { [Op.in]: [1, 2, 3, 4, 5] }
+    }, {});
   }
 };
