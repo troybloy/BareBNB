647fefb95ef8c1371cc711584621ffe4075e780d
Troy Lee <troylee@berkeley.edu>
2023-01-19 21:00:17 -0800

    changed a few modals and changed seeders for spots

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,9 @@ import { useDispatch } from "react-redux";
 import { Switch } from "react-router-dom";
 import * as sessionActions from "./store/session";
 import Navigation from "./components/Navigation";
+import HomePage from './components/Spots'
+import {Route} from 'react-router-dom'
+
 
 function App() {
   const dispatch = useDispatch();
@@ -16,6 +19,23 @@ function App() {
       <Navigation isLoaded={isLoaded} />
       {isLoaded && (
         <Switch>
+
+          <Route
+          exact
+          path='/'>
+            <HomePage />
+          </Route>
+
+
+
+
+
+
+
+
+
+
+
         </Switch>
       )}
     </>
